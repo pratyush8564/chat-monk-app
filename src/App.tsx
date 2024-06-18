e4546b3e2e33da52514d559cdea54e6721c18be8
pratyushsingh8564 <singhpratyush@seasia.in>
2024-06-18 15:39:28 +0530

    latest code

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+<div>
+  I'm trying to deploy on netlify
+</div>
+    
+  )
+}
+
+export default App
